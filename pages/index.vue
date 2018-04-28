d24e9cd6f017744a59341549bcee7c2111c30eb3
coltborg <coltborg@gmail.com>
2018-04-28 08:22:37 -0500

    Add config for tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="container">
+  <section class="">
     <div>
-      <h1 class="title">
-        identity-test
+      <h1 class="text-3xl bg-yellow text-black">
+        Identity Test
       </h1>
       <div data-netlify-identity-menu></div>
       <div data-netlify-identity-button>Login with Netlify Identity</div>
@@ -22,34 +22,7 @@ export default {
 };
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="postcss">
+@tailwind preflight;
+@tailwind utilities;
 </style>
